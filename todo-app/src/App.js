aa2bad9665c9a744f4007b58038b03febaad2e68
Erik-Hanson <ErikHanson135@gmail.com>
2020-05-10 13:04:03 -0700

    Checking in...

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -2,15 +2,21 @@ import React from 'react';
 import CreateItem from "./components/CreateItem";
 import ItemList from "./components/ItemList";
 import "bootstrap/dist/css/bootstrap.min.css"; // Bootstrap import
+import uuid from "uuid";
 //import './App.css';
 
 function App() {
   return (
-    <body>
-      <h1>Notes</h1>
-      <div id="notes-section">
-        <CreateItem />
-        <ItemList />
+    <body className="bg-dark">
+      <h1 className="text-center text-light">To Do App</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mx-auto col-md-8 mt-4">
+            <h2 className="text-center text-light">Create To Do Item</h2>
+            <CreateItem />
+            <ItemList />
+          </div>
+        </div>
       </div>
       <footer>Created By: Erik Hanson and Davis Giang</footer>
     </body>
--- todo-app/src/components/CreateItem.js
+++ todo-app/src/components/CreateItem.js
@@ -1,10 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component, createContext } from 'react'
+import './CreateItem.css';
 
 export default class CreateItem extends Component {
     render() {
         return (
-            <div>
-                <h1>Create To Do Item</h1>
+            <div className="card card-body my-3 bg-secondary">
+                <form>
+                    <div className="input-group">
+                        <div className="input-group-prepend d-inline">
+                            <div className="input-group-text bg-success">
+                                <button type="submit" className="btn btn-sm"><i className="fa fa-plus-circle"></i></button>
+                            </div>
+                        </div>
+                        <input type="text" id="addItem" className="form-control form-control-lg" placeholder="What needs to be done?" />
+                    </div>
+                </form>
             </div>
         )
     }
