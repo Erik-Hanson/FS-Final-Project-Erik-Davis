2cbb7b934cc18f92e5a3ebb6b2a9aa582ac1700e
Erik-Hanson <ErikHanson135@gmail.com>
2020-05-26 14:11:06 -0700

    checking in so I can check out another branch

--- todo-app/src/components/Item.js
+++ todo-app/src/components/Item.js
@@ -10,6 +10,8 @@ import ItemList from "./ItemList";
 import Notes from "./Notes";
 import { withRouter, useHistory } from "react-router-dom";
 import { withFirebase } from "./Firebase";
+import Accordion from "react-bootstrap/Accordion";
+import Card from "react-bootstrap/Card";
 
 const Item = (props) => {
   //const [update, doUpdate] = useState(false);
@@ -33,29 +35,64 @@ const Item = (props) => {
   //   console.log("testing");
   // }, [update])
 
+  /*
+        <Accordion>
+        {notes.map((note) => {
+          return (
+            <Card>
+              <Accordion.Toggle className="text-center" as={Card.Header} eventKey={note.id}>
+                {note.Title}
+              </Accordion.Toggle>
+              <Accordion.Collapse eventKey={note.id}>
+                <Card.Body>
+                  <div>Text: {note.Text}</div>
+                  <div>Category: {note.Category}</div>
+                  <span id="delete" className="text-danger">
+                    <button
+                      type="submit"
+                      className="btn btn-sm"
+                      onClick={(e) => deleteNote(e, note.id)}
+                    >
+                      <i className="fa fa-trash"></i>
+                    </button>
+                  </span>
+                </Card.Body>
+              </Accordion.Collapse>
+            </Card>
+          );
+        })}
+      </Accordion>
+      */
+
   return props.allNotes.map((note) => {
     return (
-      <li key={note.id} className="text-center list-group-item text-capitalize">
-        <div className="border border-secondary my-2">
-          <h5>{note.Title}</h5>
-          <div>
-            <span id="edit" className="text-success mr-2">
-              <button type="submit" className="btn btn-sm">
-                <i className="fa fa-pencil-square-o" aria-hidden="true"></i>
-              </button>
-            </span>
-            <span id="delete" className="text-danger">
-              <button
-                type="submit"
-                className="btn btn-sm"
-                onClick={() => deleteNote(note.id, note)}
-              >
-                <i className="fa fa-trash"></i>
-              </button>
-            </span>
+      <Accordion>
+        <li key={note.id} className="text-center list-group-item text-capitalize">
+          <div className="border border-secondary my-2">
+            <Accordion.Toggle className="text-center" as={Card.Header} eventKey={note.id}>
+              {note.Title}
+            </Accordion.Toggle>
+            <Accordion.Collapse eventKey={note.id}>
+              <div>
+                <span id="edit" className="text-success mr-2">
+                  <button type="submit" className="btn btn-sm">
+                    <i className="fa fa-pencil-square-o" aria-hidden="true"></i>
+                  </button>
+                </span>
+                <span id="delete" className="text-danger">
+                  <button
+                    type="submit"
+                    className="btn btn-sm"
+                    onClick={() => deleteNote(note.id, note)}
+                  >
+                    <i className="fa fa-trash"></i>
+                  </button>
+                </span>
+              </div>
+            </Accordion.Collapse>
           </div>
-        </div>
-      </li>
+        </li>
+      </Accordion>
     );
   });
 };
